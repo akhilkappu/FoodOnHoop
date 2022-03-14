840c8041268cd19c8e4af142490350d2afce44e2
akhilkappu <100773329+akhilkappu@users.noreply.github.com>
2022-03-14 21:37:49 +0530

    new

---
+++ BusinessLayer/LoginBusiness.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class LoginBusiness
+    {
+        public void GetLoginDataBL(FoodOnHoopModel onHoopModel)
+        {
+            LoginOpen open = new LoginOpen();
+            open.GetLoginData(onHoopModel);
+        }
+    }
+}
